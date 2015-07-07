3e535219f2fc8e03838030775b711ab1b9a8192e
Deamond12333 <andrei-587@mail.ru>
2015-07-07 13:27:16 +0300

    Added Operations factory

--- Calculator/Calculator/OperationsFactory.cs
+++ Calculator/Calculator/OperationsFactory.cs
@@ -6,8 +6,28 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class OperationsFactory
+    public static class OperationsFactory
     {
-        
+        public static IOperation CreateOperation(string operation)
+        {
+            switch (operation)
+            {
+                case "Multiply": return new Multiply();
+                case "Subtract": return new Subtract();
+                case "Divide": return new Divide();
+                case "Add": return new Add();
+                case "Pow": return new Pow();
+                default: throw new Exception("Unknown operation!");
+            }
+        }
+
+        public static ITrigonometria CreateTrigonometriaOperation(string operation)
+        {
+            switch (operation)
+            {
+                case "Sin": return new Sin();
+                default: throw new Exception("Unknown operation!");
+            }
+        }
     }
 }
